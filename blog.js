3ced3d54a28af4f847329777b352e07d73088cd5
0x9z <270520765+0x9z@users.noreply.github.com>
2026-08-20 22:31:13 -0400

    Update blog.js

--- blog.js
+++ blog.js
@@ -227,3 +227,16 @@ if (document.readyState === 'loading') {
 } else {
     new BlogApp();
 }
+
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('SW registered');
+            })
+            .catch(error => {
+                console.log('SW registration failed');
+            });
+    });
+}
